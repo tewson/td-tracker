ad9a1f471a260f5fa63347e5fd3278f0652a1b3b
Tewson Seeoun <tewson.seeoun@gmail.com>
2019-12-15 00:54:58 +0000

    Add basic attendance type toggle

--- src/attendance-input/AttendanceInputCalendar.jsx
+++ src/attendance-input/AttendanceInputCalendar.jsx
@@ -3,11 +3,21 @@ import format from "date-fns/format";
 
 import { Calendar } from "../calendar/Calendar.jsx";
 
+const ATTENDANCE_TYPE = {
+  SITTING: "SITTING",
+  OTHER: "OTHER"
+};
+
 export const AttendanceInputCalendar = () => {
+  const [attendanceType, setAttendanceType] = useState(ATTENDANCE_TYPE.SITTING);
   const [attendance, setAttendance] = useState({});
 
   const logAttendanceDates = () => {
     console.log(Object.keys(attendance));
+  };
+
+  const handleAttendanceTypeChange = ({ target: { value } }) => {
+    setAttendanceType(value);
   };
 
   const renderAttendanceInputDate = date => {
@@ -42,6 +52,28 @@ export const AttendanceInputCalendar = () => {
 
   return (
     <>
+      <div className="control has-text-right">
+        <label className="radio">
+          <input
+            type="radio"
+            name="attendance-type"
+            value={ATTENDANCE_TYPE.SITTING}
+            checked={attendanceType === ATTENDANCE_TYPE.SITTING}
+            onChange={handleAttendanceTypeChange}
+          />
+          Sitting
+        </label>
+        <label className="radio">
+          <input
+            type="radio"
+            name="attendance-type"
+            value={ATTENDANCE_TYPE.OTHER}
+            checked={attendanceType === ATTENDANCE_TYPE.OTHER}
+            onChange={handleAttendanceTypeChange}
+          />
+          Other
+        </label>
+      </div>
       <Calendar renderDate={renderAttendanceInputDate} />
       <div className="container has-text-right">
         <button className="button is-primary" onClick={logAttendanceDates}>
