614d06536676d40aa1944c802091993c01e69b02
Tewson Seeoun <tewson.seeoun@gmail.com>
2019-12-30 06:21:45 +0000

    Add FAQ modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
 import React, { useState } from "react";
 
+import { FAQModal } from "./FAQModal.jsx";
 import { TDSelector } from "./td-selector/TDSelector.jsx";
 import { ActivityCalendar } from "./calendar/ActivityCalendar.jsx";
 
 export const App = () => {
+  const [faqModalIsOpen, setFAQModalIsOpen] = useState(false);
   const [selectedTD, setSelectedTD] = useState();
 
   const handleTDSelect = td => {
     setSelectedTD(td);
   };
 
+  const openFAQModal = () => {
+    setFAQModalIsOpen(true);
+  };
+  const closeFAQModal = () => {
+    setFAQModalIsOpen(false);
+  };
+
   return (
-    <section className="section">
-      <div className="container">
-        <h1 className="title">TD Tracker</h1>
-        <TDSelector onSelect={handleTDSelect} />
-        {selectedTD && <ActivityCalendar td={selectedTD} />}
-      </div>
-    </section>
+    <>
+      <section className="section">
+        <div className="container">
+          <h1 className="title">
+            TD Tracker{" "}
+            <button
+              className="button is-text is-pulled-right"
+              onClick={openFAQModal}
+            >
+              FAQ
+            </button>
+          </h1>
+          <TDSelector onSelect={handleTDSelect} />
+          {selectedTD && <ActivityCalendar td={selectedTD} />}
+        </div>
+      </section>
+      <FAQModal
+        modalIsOpen={faqModalIsOpen}
+        closeModal={closeFAQModal}
+      ></FAQModal>
+    </>
   );
 };
---
+++ src/FAQModal.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export const FAQModal = ({ modalIsOpen, closeModal }) => {
+  if (!modalIsOpen) {
+    return null;
+  }
+
+  return (
+    <div className="modal faq-modal is-active">
+      <div className="modal-background" onClick={closeModal} />
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">FAQ</p>
+        </header>
+        <section className="modal-card-body">
+          <div className="content">
+            <h4>Where is the data from?</h4>
+            <p>
+              Contribution data comes from the{" "}
+              <a href="https://api.oireachtas.ie/">Oireachtas Open Data APIs</a>
+              . Attendance data comes from published{" "}
+              <a href="https://www.oireachtas.ie/en/publications/?q=&date=&term=%2Fie%2Foireachtas%2Fhouse%2Fdail%2F32&fromDate=30%2F11%2F2019&toDate=01%2F12%2F2019&topic%5B%5D=record-of-attendance">
+                records of attendance
+              </a>
+              .
+            </p>
+          </div>
+        </section>
+        <footer className="modal-card-foot">
+          <a className="button" onClick={closeModal}>
+            Close
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+};
