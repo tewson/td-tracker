9bcaf2b25e0760df59dc9cc8887647e86884e333
Tewson Seeoun <tewson.seeoun@gmail.com>
2019-12-30 04:06:19 +0000

    Move contribution modal content section into components

--- src/calendar/ContributionModal.jsx
+++ src/calendar/ContributionModal.jsx
@@ -9,6 +9,83 @@ export const ContributionModal = ({ data, closeModal }) => {
   const { date, debates, divisions } = data;
   const formattedDate = format(date, "yyyy-MM-dd");
 
+  const DebateSection = () => {
+    return (
+      <>
+        <h3>Debates</h3>
+        <ul>
+          {debates
+            .map(debate => {
+              const {
+                debateRecord: {
+                  debateSections,
+                  house: { chamberType, houseCode, committeeCode }
+                }
+              } = debate;
+
+              const chamber =
+                chamberType === "house" ? houseCode : committeeCode;
+
+              return debateSections.map(({ debateSection }) => {
+                const { uri, debateSectionId, showAs } = debateSection;
+
+                const debateSectionIdForUrl = debateSectionId.replace(
+                  "dbsect_",
+                  ""
+                );
+
+                return (
+                  <li key={uri}>
+                    <a
+                      href={`https://www.oireachtas.ie/en/debates/debate/${chamber}/${formattedDate}/${debateSectionIdForUrl}`}
+                    >
+                      {showAs}
+                    </a>
+                  </li>
+                );
+              });
+            })
+            .flat(Infinity)}
+        </ul>
+      </>
+    );
+  };
+
+  const DivisionSection = () => {
+    return (
+      <>
+        <h3>Divisions</h3>
+        <ul>
+          {divisions.map(division => {
+            const {
+              uri,
+              debate,
+              voteId,
+              house: { chamberType, committeeCode, houseCode, houseNo }
+            } = division;
+
+            const voteIdForUrl = voteId.replace("vote_", "");
+
+            const divisionDebatePath =
+              chamberType === "committee"
+                ? `/${houseCode}/${houseNo}/${committeeCode}/${formattedDate}/${voteIdForUrl}`
+                : `/${houseCode}/${houseNo}/${formattedDate}/${voteIdForUrl}`;
+
+            return (
+              <li key={uri}>
+                <a
+                  href={`https://www.oireachtas.ie/en/debates/vote${divisionDebatePath}`}
+                >
+                  {debate.showAs}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </>
+    );
+  };
+
   return (
     <div className="modal contribution-modal is-active">
       <div className="modal-background" onClick={closeModal} />
@@ -18,69 +95,8 @@ export const ContributionModal = ({ data, closeModal }) => {
         </header>
         <section className="modal-card-body">
           <div className="content">
-            <h3>Debates</h3>
-            <ul>
-              {debates
-                .map(debate => {
-                  const {
-                    debateRecord: {
-                      debateSections,
-                      house: { chamberType, houseCode, committeeCode }
-                    }
-                  } = debate;
-
-                  const chamber =
-                    chamberType === "house" ? houseCode : committeeCode;
-
-                  return debateSections.map(({ debateSection }) => {
-                    const { uri, debateSectionId, showAs } = debateSection;
-
-                    const debateSectionIdForUrl = debateSectionId.replace(
-                      "dbsect_",
-                      ""
-                    );
-
-                    return (
-                      <li key={uri}>
-                        <a
-                          href={`https://www.oireachtas.ie/en/debates/debate/${chamber}/${formattedDate}/${debateSectionIdForUrl}`}
-                        >
-                          {showAs}
-                        </a>
-                      </li>
-                    );
-                  });
-                })
-                .flat(Infinity)}
-            </ul>
-            <h3>Divisions</h3>
-            <ul>
-              {divisions.map(division => {
-                const {
-                  uri,
-                  debate,
-                  voteId,
-                  house: { chamberType, committeeCode, houseCode, houseNo }
-                } = division;
-
-                const voteIdForUrl = voteId.replace("vote_", "");
-
-                const divisionDebatePath =
-                  chamberType === "committee"
-                    ? `/${houseCode}/${houseNo}/${committeeCode}/${formattedDate}/${voteIdForUrl}`
-                    : `/${houseCode}/${houseNo}/${formattedDate}/${voteIdForUrl}`;
-
-                return (
-                  <li key={uri}>
-                    <a
-                      href={`https://www.oireachtas.ie/en/debates/vote${divisionDebatePath}`}
-                    >
-                      {debate.showAs}
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
+            {debates.length > 0 && <DebateSection />}
+            {divisions.length > 0 && <DivisionSection />}
           </div>
         </section>
         <footer className="modal-card-foot">
