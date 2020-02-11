eadec0ff6479799545316304aa0b120a15ea4616
Tewson Seeoun <tewson.seeoun@gmail.com>
2020-02-11 01:32:27 +0000

    Add ActivityCalendarContainer
    that accepts TD memberCode and renders ActivityCalendar.
    
    This will be helpful when we take the TD memberCode from the URL.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
 import { hot } from "react-hot-loader/root";
 
+import dailMembers from "../data/dail/32/members.json";
+
 import { FAQModal } from "./FAQModal.jsx";
 import { TDSelector } from "./td-selector/TDSelector.jsx";
 import { ActivityCalendar } from "./calendar/ActivityCalendar.jsx";
 
+const ActivityCalendarContainer = ({ tdMemberCode }) => {
+  const td = dailMembers.results
+    .map(result => result.member)
+    .find(member => member.memberCode === tdMemberCode);
+
+  if (!td) {
+    return null;
+  }
+
+  return <ActivityCalendar td={td} />;
+};
+
 const App = () => {
   const [faqModalIsOpen, setFAQModalIsOpen] = useState(false);
-  const [selectedTD, setSelectedTD] = useState();
+  const [selectedTDMemberCode, setSelectedTDMemberCode] = useState();
 
   const handleTDSelect = td => {
-    setSelectedTD(td);
+    setSelectedTDMemberCode(td.memberCode);
   };
 
   const openFAQModal = () => {
@@ -34,13 +48,15 @@ const App = () => {
             </button>
           </h1>
           <TDSelector onSelect={handleTDSelect} />
-          {!selectedTD && (
+          {!selectedTDMemberCode && (
             <p>
               Search for a TD and view their attendance and contributions in
               2019.
             </p>
           )}
-          {selectedTD && <ActivityCalendar td={selectedTD} />}
+          {selectedTDMemberCode && (
+            <ActivityCalendarContainer tdMemberCode={selectedTDMemberCode} />
+          )}
         </div>
       </section>
       <FAQModal
