0a0f587d4d97f3698d25743262cb324ea3bcfddc
Tewson Seeoun <tewson.seeoun@gmail.com>
2020-03-04 23:24:09 +0000

    Reduce code duplication on year date range

--- src/divisions/api.js
+++ src/divisions/api.js
@@ -1,20 +1,19 @@
 import axios from "axios";
 import format from "date-fns/format";
-import startOfYear from "date-fns/startOfYear";
-import endOfYear from "date-fns/endOfYear";
+
+import { getStartAndEndOfYear } from "../utils.js";
 
 let cachedAllDailDivisions;
 
 export const fetchDivisions = async (year, td) => {
-  const yearNumber = parseInt(year, 10);
-  const currentDate = new Date(yearNumber, 0, 1);
+  const { startOfYear, endOfYear } = getStartAndEndOfYear(year);
 
   const {
     data: { results }
   } = await axios.get("https://api.oireachtas.ie/v1/divisions", {
     params: {
-      date_start: format(startOfYear(currentDate), "yyyy-MM-dd"),
-      date_end: format(endOfYear(currentDate), "yyyy-MM-dd"),
+      date_start: format(startOfYear, "yyyy-MM-dd"),
+      date_end: format(endOfYear, "yyyy-MM-dd"),
       member_id: td.uri,
       limit: 10000
     }
@@ -28,15 +27,14 @@ export const fetchAllDailDivisions = async (houseNumber, year) => {
     return cachedAllDailDivisions;
   }
 
-  const yearNumber = parseInt(year, 10);
-  const currentDate = new Date(yearNumber, 0, 1);
+  const { startOfYear, endOfYear } = getStartAndEndOfYear(year);
 
   const {
     data: { results }
   } = await axios.get("https://api.oireachtas.ie/v1/divisions", {
     params: {
-      date_start: format(startOfYear(currentDate), "yyyy-MM-dd"),
-      date_end: format(endOfYear(currentDate), "yyyy-MM-dd"),
+      date_start: format(startOfYear, "yyyy-MM-dd"),
+      date_end: format(endOfYear, "yyyy-MM-dd"),
       chamber_id: `https://data.oireachtas.ie/ie/oireachtas/house/dail/${houseNumber}`,
       chamber: "dail",
       limit: 10000
