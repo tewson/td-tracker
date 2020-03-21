77f6093cfc2186ea55cde979c8896f7a67aa3c0d
Tewson Seeoun <tewson.seeoun@gmail.com>
2020-03-21 17:07:12 +0000

    Add no. of sitting days in period to attendance input form

--- src/attendance-input/constants.js
+++ src/attendance-input/constants.js
@@ -2,3 +2,5 @@ export const DEFAULT_ATTENDANCE_SOURCE_URL =
   "https://data.oireachtas.ie/ie/oireachtas/members/recordAttendanceForTaa/2020/2020-03-01_deputies-verification-of-attendance-for-the-payment-of-taa-1-jan-to-14-jan-2020_en.pdf";
 
 export const DEFAULT_ATTENDANCE_RECORD_DATE = "2020-01-14";
+
+export const DEFAULT_NUMBER_OF_SITTING_DAYS_IN_PERIOD = 0;
